1dcdefcf9679b46f79f208428b8bfda493ce55e0
kereh <kreh.whoami@gmail.com>
2024-06-07 11:15:48 +0800

    update: add nim validation if user nim more than 8 digits

--- src/schemas/login-schema.ts
+++ src/schemas/login-schema.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  nim: z.string().min(8, { message: "Masukan NIM dengan benar" }),
+  nim: z
+    .string()
+    .min(8, { message: "Masukan NIM dengan benar" })
+    .max(8, { message: "NIM Tidak Boleh Lebih Dari 8 Digit" }),
 });
